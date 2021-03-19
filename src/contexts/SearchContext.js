b96576f2151fda0828ce14da9cffeda0a5bdea66
datokey <datokey11@gmail.com>
2021-03-19 08:01:51 +0700

    fix warning

--- src/contexts/SearchContext.js
+++ src/contexts/SearchContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import axios from 'axios';
 
 export const SearchContext = createContext();
--- src/page/Genres.js
+++ src/page/Genres.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { SearchContext } from "../contexts/SearchContext";
 import { Link } from "react-router-dom";
 
